4e51c8a7aa323936052235ccd6eb4618f0abd25e
ONovosolov <ONovosolov@ONOVOSIOLOV-NB>
2020-12-09 10:55:25 +0200

    Comit

---
+++ XO/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XO
+{
+    public class Player
+    {
+        public Player(string name, char symbol)
+        {
+            name = PlayerName;
+            PlayerSymbol = symbol;
+        }
+
+        public string PlayerName { get; }
+        public char PlayerSymbol { get; }
+    }
+}
